f157a4a8460b8ad2269bc0ee3dddbc5c2af98560
IldarMin <ildarcold@gmail.com>
2016-12-06 20:12:09 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+var restart = document.getElementById('button_correct');
+restart.click();
+var task_x = +document.getElementById('task_x').innerHTML;
+var task_op = document.getElementById('task_op').innerHTML;
+var task_y = +document.getElementById('task_y').innerHTML;
+var task_res = document.getElementById('task_res').innerHTML;
+
+function runIt() {
+  task_x = +document.getElementById('task_x').innerHTML;
+  task_op = document.getElementById('task_op').innerHTML;
+  task_y = +document.getElementById('task_y').innerHTML;
+  task_res = +document.getElementById('task_res').innerHTML;
+  
+  if (task_op == '+') {
+    if (task_x+task_y == task_res) {
+      doTrue();
+    }else {
+      doFalse();
+    }
+  }
+  if (task_op == '–') {
+    if (task_x-task_y == task_res) {
+      doTrue();
+    }else {
+      doFalse();
+    }
+  }
+  if (task_op == '/') {
+    if (task_x/task_y == task_res) {
+      doTrue();
+    }else {
+      doFalse();
+    }
+  }
+  if (task_op == '×') {
+    if (task_x*task_y == task_res) {
+      doTrue();
+    }else {
+      doFalse();
+    }
+  }
+};
+
+runIt();
+
+function doTrue() {
+  setTimeout(function () {
+    document.getElementById('button_correct').click();
+    runIt();
+  }, 1000);
+};
+function doFalse() {
+  setTimeout(function () {
+    document.getElementById('button_wrong').click();
+    runIt();
+  }, 1000);
+};
